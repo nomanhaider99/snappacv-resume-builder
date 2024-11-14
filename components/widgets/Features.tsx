eeba7ad74872470b627caf65c39c98ef6e70001b
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2024-11-14 13:17:07 +0500

    design changes

--- components/widgets/Features.tsx
+++ components/widgets/Features.tsx
@@ -6,14 +6,14 @@ const Features = () => {
   return (
     <div className='w-full px-14 py-10 flex flex-col gap-14'>
         {/* Headings */}
-        <div className='w-full flex justify-between items-center'>
+        <div className='w-full flex flex-col md:flex-row justify-between items-center'>
             {/* Right */}
-            <div className='w-[40%] uppercase text-4xl font-extrabold tracking-tighter text-black leading-none'>Why should you build resume here</div>
+            <div className='md:w-[40%] w-full uppercase text-4xl font-extrabold tracking-tighter text-black leading-none'>Why should you build resume here</div>
             {/* Left */}
-            <div className='w-[60%] text-zinc-800 tracking-tighter leading-5'>Build your resume with us to stand out in today’s competitive job market. We don’t just create templates; we design personalized resumes that highlight your unique skills, experience, and achievements, helping you make a strong impression on potential employers. Our team understands the latest industry trends and the elements that hiring managers are looking for, ensuring your resume is optimized to pass through applicant tracking systems and grab attention. With our expertise, you’ll have a resume that not only looks professional but also effectively tells your story, making it easier for you to land the job you want.</div>
+            <div className='md:w-[60%] w-full text-zinc-800 tracking-tighter leading-5'>Build your resume with us to stand out in today’s competitive job market. We don’t just create templates; we design personalized resumes that highlight your unique skills, experience, and achievements, helping you make a strong impression on potential employers. Our team understands the latest industry trends and the elements that hiring managers are looking for, ensuring your resume is optimized to pass through applicant tracking systems and grab attention. With our expertise, you’ll have a resume that not only looks professional but also effectively tells your story, making it easier for you to land the job you want.</div>
         </div>
         {/* Cards */}
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 items-center'>
             {
                 featuresData.map((feature, index) => (
                     <FeatureCard
--- components/widgets/Hero.tsx
+++ components/widgets/Hero.tsx
@@ -6,7 +6,7 @@ import Button from '../ui/Button'
 
 const Hero = () => {
   return (
-    <div className='px-10 w-full flex flex-col gap-5 items-center pt-24 pb-36'>
+    <div className='md:px-10 px-4 w-full flex flex-col gap-5 items-center pt-24 pb-36'>
         {/* Upper */}
         <div className='flex justify-between items-center px-5 gap-10'>
             {/* Left shape */}
@@ -23,7 +23,7 @@ const Hero = () => {
                 {/* Heading */}
                 <div className='uppercase text-5xl text-center leading-none font-extrabold text-black tracking-tighter'>The Only Resume Bulder that increase your job accuration</div>
                 {/* para */}
-                <div className='text-lg text-zinc-800 text-center tracking-tighter leading-6 w-1/2'>Build a resume that stands out with ease. Our tool helps you create a professional, personalized resume, highlighting your skills and achievements to make a lasting impression.</div>
+                <div className='text-lg text-zinc-800 text-center tracking-tighter leading-6 md:w-1/2 w-full'>Build a resume that stands out with ease. Our tool helps you create a professional, personalized resume, highlighting your skills and achievements to make a lasting impression.</div>
             </div>
             {/* Right shape */}
             <div>
